80688ae6e8fd9e2ce18e2bd61878c95594bb438b
neerajm <admin@admins-Mac-3.local>
2014-08-15 11:45:43 -0700

    replaced developer names with company name

--- MasterSummerProject/BIDEmployeeHandbook.m
+++ MasterSummerProject/BIDEmployeeHandbook.m
@@ -2,7 +2,7 @@
 //  BIDEmployeeHandbook.m
 //  MasterSummerProject
 //
-//  Created by Nick Woodward on 8/12/14.
+//  Created by Software Merchant Inc. on 8/12/14.
 //  Copyright (c) 2014 Softwaremerchant. All rights reserved.
 //
 
