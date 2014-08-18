be675856dfe1ebbc8e9d27758542068b8398ff4c
neerajm <admin@admins-Mac-5.local>
2014-08-18 13:33:51 -0700

    change password page
    new view controller added for change password functionality.

---
+++ MasterSummerProject/BIDChangePassword.h
@@ -0,0 +1,17 @@
+//
+//  BIDChangePassword.h
+//  MasterSummerProject
+//
+//  Created by admin on 8/18/14.
+//  Copyright (c) 2014 Softwaremerchant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDChangePassword : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *newPassword;
+@property (strong, nonatomic) IBOutlet UITextField *confirmPassword;
+
+- (IBAction)savePassword;
+
+@end
